ee3bb7ede30be74350a0151b3d5be89f5bd2a7f7
DMelinaPino <166168337+DMelinaPino@users.noreply.github.com>
2024-05-10 15:10:16 -0300

    Add files via upload

---
+++ cuidadoconel consoleloong.js
@@ -0,0 +1,13 @@
+//Función: cuidadoConElConsoleLog
+function cuidadoConElConsoleLog(nombre) {
+    console.log(nombre);
+    return nombre;
+  }
+  //abrir una terminal, colocar node
+  //copiar la funcion y pegarla a la terminal
+  //escribir cuidadoconelconsolelog("camilo")
+  //Camilo
+  //'Camilo'
+  //en conclusion retorna 2 veces camilo con y sin comillas
+
+
